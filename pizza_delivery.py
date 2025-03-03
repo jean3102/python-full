249dc0ebc4d01eeb52dffa8c59792f29443b5769
Jean Carlos Luciano Ortega <56600646+jean3102@users.noreply.github.com>
2025-03-02 21:50:38 -0500

    chore(pizza_delivery): requirement note

---
+++ pizza_delivery.py
@@ -0,0 +1,8 @@
+# Write a Pizza Delivery Program Congratulations, you've got a job at Python Pizzal Your first job is to build an automatic pizza order program.
+# Based on a user's order, work out their final bill.
+# Small pizza (S): $15
+# Medium pizza (M): $20
+# Large pizza (L): $25
+# Add pepperoni for small pizza (Y or N): +$2
+# Add pepperoni for medium or large pizza (Y or N): +$3
+# Add extra cheese for any size pizza (Y or N): +$1
