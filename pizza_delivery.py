b9abe521d32c5730394b8a5c212d916838ab238d
jean carlos <jeancarlos3102@gmail.com>
2025-03-03 11:39:37 -0500

    feat(pizza_delivery): task completed

--- pizza_delivery.py
+++ pizza_delivery.py
@@ -1,4 +1,5 @@
-# Write a Pizza Delivery Program Congratulations, you've got a job at Python Pizzal Your first job is to build an automatic pizza order program.
+# Write a Pizza Delivery Program Congratulations,
+# you've got a job at Python Pizza Your first job is to build an automatic pizza order program.
 # Based on a user's order, work out their final bill.
 # Small pizza (S): $15
 # Medium pizza (M): $20
@@ -6,3 +7,35 @@
 # Add pepperoni for small pizza (Y or N): +$2
 # Add pepperoni for medium or large pizza (Y or N): +$3
 # Add extra cheese for any size pizza (Y or N): +$1
+
+size_choice = input("what size pizza do you want? S, M, L: ").lower()
+peperoni = input("Do you want peperoni on your pizza? Y or N ").lower()
+extra_cheese = input("Do you want extra cheese? Y or N ").lower()
+TOTAL_PRICE = 0
+SMALL_PIZZA_PRICE = 15
+MEDIUM_PIZZA_PRICE = 20
+LARGE_PIZZA_PRICE = 25
+
+# * work out how much they need to pay based on their size choice.
+if size_choice == "s":
+    TOTAL_PRICE = SMALL_PIZZA_PRICE
+    if peperoni == "y":
+        TOTAL_PRICE += 2
+        print(f'''{TOTAL_PRICE=}''')
+
+if size_choice == "m":
+    TOTAL_PRICE = MEDIUM_PIZZA_PRICE
+
+if size_choice == "l":
+    TOTAL_PRICE = LARGE_PIZZA_PRICE
+
+if extra_cheese == "y":
+    TOTAL_PRICE += 1
+
+if peperoni == "y":
+    TOTAL_PRICE += 3
+
+if peperoni == "y" and size_choice != "S":
+    TOTAL_PRICE += 1
+
+print(f"Your final bill is: {TOTAL_PRICE}")
