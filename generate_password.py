e8593db3afcd7e6892970673f0e66709ee2ca9e4
jean carlos <jeancarlos3102@gmail.com>
2025-03-05 08:40:13 -0500

    chore(generate_password): requirement note

---
+++ generate_password.py
@@ -0,0 +1,13 @@
+LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+           'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+           ]
+
+SYMBOLS = ["!", "@", "#", "$", "%", "^", "&", "*"]
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+print("Welcome to the Password Generator!")
+
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
