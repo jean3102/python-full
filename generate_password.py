2c6f9f6dc091de3ff316352fcf5285c51aef4321
jean carlos <jeancarlos3102@gmail.com>
2025-03-06 18:49:01 -0500

    feat(generate_password): task done

--- generate_password.py
+++ generate_password.py
@@ -1,13 +1,25 @@
-LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-           'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-           'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-           ]
+import random
 
-SYMBOLS = ["!", "@", "#", "$", "%", "^", "&", "*"]
-NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+SYMBOLS = "!@#$%^&*()"
+NUMBERS = "0123456789"
 
 print("Welcome to the Password Generator!")
 
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
+
+
+def get_random_elements(count, elements):
+    return random.choices(elements, k=count)
+
+
+char_letters = get_random_elements(nr_letters, LETTERS)
+char_symbols = get_random_elements(nr_symbols, SYMBOLS)
+char_numbers = get_random_elements(nr_numbers, NUMBERS)
+
+password_list = char_letters + char_symbols + char_numbers
+random.shuffle(password_list)
+PASSWORD = "".join(password_list)
+print(f'''{PASSWORD=}''')
