e00cc7ed4f8c72d640846f9d012ae168e8e7955e
jean carlos <jeancarlos3102@gmail.com>
2025-03-13 19:39:40 -0400

    feat(hangman_game): task done

---
+++ hangman_game_data.py
@@ -0,0 +1,64 @@
+HANGMAN_STAGES = [
+    '''
+    -----
+    |   |
+        |
+        |
+        |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+        |
+        |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+    |   |
+        |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+   /|   |
+        |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+   /|\\  |
+        |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+   /|\\  |
+   /    |
+        |
+    -----''',
+    '''
+    -----
+    |   |
+    O   |
+   /|\\  |
+   / \\  |
+        |
+    -----'''
+]
+WORD_LIST = [
+    "apple", "banana", "cherry", "date", "elderberry",
+    "fig", "grape", "honeydew", "kiwi", "lemon",
+    "mango", "nectarine", "orange", "papaya", "quince",
+    "raspberry", "strawberry", "tangerine", "ugli", "watermelon"
+]
